fee7e3b84cc65cdffa5310164f9f26b5fb27264b
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2023-10-15 13:59:35 +0200

    Encje i walidacja

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class IndexController extends AbstractController
 {
@@ -14,5 +18,46 @@ class IndexController extends AbstractController
         return $this->render('index.html.twig', [
             'hello' => 'World!'
         ]);
+    }
+
+    #[Route(path: '/register', name: 'register', methods: ['GET'])]
+    public function register(
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,
+    ): Response
+    {
+        $user = new User();
+        $plaintextPassword = 'haslo';
+
+        $user
+            ->setEmail('to nie jest email!!!11');
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        $errors = $validator->validate($user);
+
+        
+
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+
+
+
+            return new Response(implode("<br>", $errorMessages), Response::HTTP_BAD_REQUEST);
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response("Registered!");
+
     }
 }
